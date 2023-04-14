9ab6ea2a19c2118d1a8bb3974b436eb5c2caf6d4
KoolTheba <gomez.teba@gmail.com>
2023-04-14 15:42:51 +0200

    feat: added react router set up to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
