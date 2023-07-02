ce4cd328e07432265291acf8a62391162ec58144
Theba Gomez <26543236+KoolTheba@users.noreply.github.com>
2023-07-02 13:35:20 +0200

    chore: linting

--- cypress/e2e/visualizer.cy.ts
+++ cypress/e2e/visualizer.cy.ts
@@ -1,18 +1,36 @@
 describe("Visualizer", () => {
   it("should render error when organization or repository does not exist", () => {
-    cy.intercept('GET', 'https://api.securityscorecards.dev/projects/github.com/nodejs/node', {
-      statusCode: 404
-    }).as('getData')
+    cy.intercept(
+      "GET",
+      "https://api.securityscorecards.dev/projects/github.com/nodejs/node",
+      {
+        statusCode: 404,
+      }
+    ).as("getData");
 
-    cy.visit("localhost:3000/openssf-scorecard-api-visualizer/#/projects/github.com/nodejs/node");
-    cy.wait('@getData') // first try as react query behaves
-    cy.wait('@getData') // second try
-    cy.wait('@getData') // third try
-    cy.get('h1', {timeout: 10000}).should('contain', 'An error ocurred. Please refresh/try again.')
-    cy.get('p', {timeout: 10000}).should('contain', "Please check if the org/repository exists on GitHub.")
+    cy.visit(
+      "localhost:3000/openssf-scorecard-api-visualizer/#/projects/github.com/nodejs/node"
+    );
+    cy.wait("@getData"); // first try as react query behaves
+    cy.wait("@getData"); // second try
+    cy.wait("@getData"); // third try
+    cy.get("h1", { timeout: 10000 }).should(
+      "contain",
+      "An error ocurred. Please refresh/try again."
+    );
+    cy.get("p", { timeout: 10000 }).should(
+      "contain",
+      "Please check if the org/repository exists on GitHub."
+    );
   });
 
   it.todo("should render error when the commit hash does not exist", () => {});
-  it.todo("should render a valid scoredata visualization for the latest reported commit", () => {});
-  it.todo("should render a valid scoredata visualization for an specific reported commit", () => {});
+  it.todo(
+    "should render a valid scoredata visualization for the latest reported commit",
+    () => {}
+  );
+  it.todo(
+    "should render a valid scoredata visualization for an specific reported commit",
+    () => {}
+  );
 });
